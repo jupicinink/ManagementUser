8f390fb379a802047da7c8e53e32cd73215c62d2
jupicinink <juliapicinini.2020@gmail.com>
2026-06-10 19:22:24 -0300

    feat: adiciona Classes Perfil e User

---
+++ ManagementUser/Models/Perfil.cs
@@ -0,0 +1,10 @@
+﻿namespace ManagementUser.Models;
+
+public class Perfil
+{
+    public int Id { get; set; }
+    public required string Nome { get; set; } // Ex: "Admin","Cliente", "Gerente"
+public string? Descricao { get; set; }
+    // Relacionamento: Um perfil pode ter vários usuários
+    public List<User> Usuarios { get; set; } = new();
+}
---
+++ ManagementUser/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace ManagementUser.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public required string Nome { get; set; }
+    public required string Email { get; set; }
+    public required string SenhaHash { get; set; }
+    public DateTime DataCriacao { get; set; } = DateTime.UtcNow;
+    // Chave estrangeira para Perfil
+    public int PerfilId { get; set; }
+    // Relacionamento: Um usuário pertence a um perfil
+    public Perfil? Perfil { get; set; }
+}
