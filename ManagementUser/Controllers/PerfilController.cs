45bc949ee5e7abcb8cf9d11c50a764aaa1f25b5e
jupicinink <juliapicinini.2020@gmail.com>
2026-06-10 20:33:57 -0300

    feat: Adiciona CONTROLLERS de Usuário e Perfil

---
+++ ManagementUser/Controllers/PerfilController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ManagementUser.DTOs;
+using ManagementUser.Services;
+[ApiController]
+[Route("api/[controller]")]
+public class PerfisController : ControllerBase
+{
+    private readonly PerfilService _perfilService;
+    public PerfisController(PerfilService perfilService)
+    {
+
+        _perfilService = perfilService;
+    }
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody]
+PerfilDto.PerfilCreateRequest dto)
+    {
+        try
+        {
+            var perfil = await _perfilService.CreatePerfil(dto);
+            return CreatedAtAction(nameof(GetById), new
+            {
+                id =
+            perfil.Id
+            }, perfil);
+        }
+        catch (KeyNotFoundException ex)
+        {
+            return NotFound(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        var perfil = await _perfilService.GetPerfilById(id);
+        return perfil != null ? Ok(perfil) : NotFound();
+    }
+}
---
+++ ManagementUser/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ManagementUser.Services;
+using ManagementUser.DTOs;
+namespace ManagementUser.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly UserService _userService;
+    public UsersController(UserService userService)
+    {
+        _userService = userService;
+    }
+    [HttpPost]
+    public async Task<IActionResult> CreateUser([FromBody]
+UserDto.UserCreateRequest dto)
+    {
+        try
+        {
+            var user = await _userService.CreateUser(dto);
+            return CreatedAtAction(nameof(GetUser), new { id = user.Id },
+            user);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message); // Ou use um middleware de erros
+        }
+    }
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetUser(int id)
+    {
+        var user = await _userService.GetUserById(id);
+        return user != null ? Ok(user) : NotFound();
+    }
+}
