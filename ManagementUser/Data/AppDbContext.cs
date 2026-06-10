811229cccbd7373692b7c311300e4a833c00889e
jupicinink <juliapicinini.2020@gmail.com>
2026-06-10 19:41:48 -0300

    feat: adiciona Classe de Suporte a Dados AppDbContext

--- ManagementUser/Data/AppDbContext.cs
+++ ManagementUser/Data/AppDbContext.cs
@@ -1,6 +1,40 @@
-﻿namespace ManagementUser.Data
+﻿using ManagementUser.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+namespace ManagementUser.Data;
+
+public class AppDbContext : DbContext
 {
-    public class AppDbContext
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    { }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Perfil> Perfis { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        // Configuração do relacionamento
+        modelBuilder.Entity<User>()
+        .HasOne(u => u.Perfil)
+        .WithMany(p => p.Usuarios)
+        .HasForeignKey(u => u.PerfilId)
+        .OnDelete(DeleteBehavior.Restrict); // Evita deletar perfis com        usuários vinculados
+    // Seed inicial de perfis
+modelBuilder.Entity<Perfil>().HasData(
+new Perfil
+{
+    Id = 1,
+    Nome = "Admin",
+    Descricao = "Administrador do sistema" },
+new Perfil
+{
+    Id = 2,
+    Nome = "Cliente",
+    Descricao = "Usuário comum"
+},
+    new Perfil
+    {
+        Id = 3,
+        Nome = "Gerente",
+        Descricao = "Gerente de operações" }
+);
     }
 }
